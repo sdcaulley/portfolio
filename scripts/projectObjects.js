e372f0c9a7f281df85af9a5d26b9f4e26409fac2
sdcaulley <stephanie.caulley@gmail.com>
2016-09-20 21:53:50 -0700

    rewrote most of the css, added jQuery

---
+++ scripts/projectObjects.js
@@ -0,0 +1,26 @@
+var codingProjects = [
+  {
+    title: 'sdcaulley.com',
+    projectUrl: 'http://sdcaulley.com',
+    projectImage: 'img/sdcaulleyScreenShot.png',
+    projectCompleted: '2014-02-25',
+    projectDescription: 'Wordpress blog featuring theology',
+    skillShowcased: ['Wordpress', 'themeing']
+  },
+  {
+    title: 'sdcaulleyoriginals.com',
+    projectUrl: 'http://sdcaulleyoriginals.com',
+    projectImage: 'img/sdcOriginalsScreenShot.png',
+    projectCompleted: '2015-04-15',
+    projectDescription: 'Wordpress blog for crafting and sewing',
+    skillShowcased: ['Wordpress', 'themeing']
+  },
+  {
+    title: 'Market Analysis Application',
+    projectUrl: 'https://sdcaulley.github.io/market-analysis-app/',
+    projectImage: 'img/busMallScreenShot.png',
+    projectCompleted: '2016-08-03',
+    projectDescription: 'JavaScript application for recording user votes.',
+    skillShowcased: ['JavaScript', 'HTML', 'CSS']
+  },
+];
